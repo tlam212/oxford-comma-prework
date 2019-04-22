6a529ad0cac72e5734d8543964afa73393c40d3f
Toni Lamont <tlam212@gmail.com>
2019-04-22 19:05:37 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
 if array.length == 1 
   array[0]
-else array.length < 3
-puts array.join(',') + 'and ' + array[-1]
+elsif array.length == 2
+puts array.join(' and ') 
 end
 end
